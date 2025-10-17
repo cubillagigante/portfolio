086c0a13706ca3a2fad86534ca561e9bebaee6a7
Diego <diegocubilla2001@gmail.com>
2025-10-17 13:30:23 -0300

    correccion de astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cubillagigante.github.io/portfolio",
-  base: "/portfolio",
+  base: "/",
   integrations: [tailwind(), icon()]
 });
